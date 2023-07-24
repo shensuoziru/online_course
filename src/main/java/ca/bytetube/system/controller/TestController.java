74571a29e8699165230a06c04f348da898009e37
shensuoziru <shsh537@163.com>
2023-07-24 23:11:12 +0800

    environment

---
+++ src/main/java/ca/bytetube/system/controller/TestController.java
@@ -0,0 +1,24 @@
+package ca.bytetube.system.controller;
+import ca.bytetube.system.domain.Test;
+import ca.bytetube.system.service.TestService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+public class TestController {
+
+    @Resource
+    private TestService testService;
+
+    @RequestMapping("/test")
+    public List<Test> test() {
+        return testService.list();
+    }
+
+
+}
+
+
---
+++ src/main/java/ca/bytetube/system/mapper/TestMapper.java
@@ -0,0 +1,10 @@
+package ca.bytetube.system.mapper;
+
+import ca.bytetube.system.domain.Test;
+
+import java.util.List;
+
+public interface TestMapper {
+    
+    List<Test> list();
+}
